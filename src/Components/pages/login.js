de7f8ef3579855338ba65d86dfcbfdf6710329ef
Joshua Veal-Briscoe <jkv2c9@mail.umsl.edu>
2018-01-20 22:35:38 -0600

    Added flexbox. Fixed login spacing.

--- src/Components/pages/login.js
+++ src/Components/pages/login.js
@@ -1,38 +1,60 @@
 import React, { Component } from 'react'
-import Paper from 'material-ui/Paper';
-import Divider from 'material-ui/Divider';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import _default from '../../Assets/scss/_config.scss';
+import { Grid, Row, Col } from 'react-flexbox-grid'
+import Paper from 'material-ui/Paper'
+import Divider from 'material-ui/Divider'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+import _default from '../../Assets/scss/_config.scss'
 
 class login extends Component {
   render() {
-      const style = {
-        height: 100,
-        width: 200,
-        margin: 5,
-        align: 'center',
-        textAlign: 'center',
-        display: 'inline-block',
-        marginRight: 20
-      };
+      // const style = {
+      //   height: 100,
+      //   width: 200,
+      //   margin: 5,
+      //   align: 'center',
+      //   textAlign: 'center',
+      //   display: 'inline-block',
+      //   marginRight: 20
+      // };
       const buttonStyle = {
 
 
       }
     return (
-    	<div className="loginCard">
-      <Paper zDepth={2}>
-       <TextField hintText="Username" style={style} underlineShow={false} id="user" />
-       <Divider />
-       <TextField hintText="Password" style={style} underlineShow={false} id="pass" />
-       <Divider />
-       <RaisedButton label="Login"  backgroundColor="darkGrey" href="/Dashboard" style={buttonStyle}   />
-      </Paper>
-      <br/>
-      No account no problem! <a href="/Create">Create One</a>
-    	  </div>
-
+                  <div>
+      <Grid fluid>
+        <Row>
+          <Col xsOffset={2} xs={8}>
+              <Paper zDepth={2}>
+                <TextField hintText="Username" underlineShow={false} id="user" />
+                <Divider />
+              </Paper>
+          </Col>
+        </Row>
+        <Row>
+          <Col xsOffset={2} xs={8}>
+            <Paper zDepth={2}>
+                <TextField hintText="Password" underlineShow={false} id="pass" />
+                <Divider />
+                </Paper>
+          </Col>
+        </Row>
+        <Row center="xs">
+          <Col xs={8}>
+          <Paper zDepth={2}>
+                <RaisedButton label="Login"  backgroundColor="darkGrey" href="/Dashboard" style={buttonStyle}   />
+                </Paper>
+          </Col>
+        </Row>
+        <Row>
+          <Col xsOffset={2} xs={8}>
+              <br/>
+                No account no problem! <a href="/Create">Create One</a>
+          </Col>
+        </Row>
+      </Grid>
+            </div>
 
 
     )
